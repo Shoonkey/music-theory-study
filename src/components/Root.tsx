b929140efb5c18652648dc297bba429da06b9a27
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-09-29 21:44:30 -0300

    feat: added back button to non-home pages

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Center,
   Flex,
   IconButton,
@@ -8,8 +9,8 @@ import {
   VisuallyHidden,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-import { SunHorizon, MoonStars } from "@phosphor-icons/react";
-import { Route, Routes } from "react-router-dom";
+import { SunHorizon, MoonStars, ArrowLeft } from "@phosphor-icons/react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 import useAppSettings from "../hooks/useAppSettings";
 import Homepage from "../pages/Homepage";
@@ -20,53 +21,62 @@ function Root() {
   const { t, i18n } = useTranslation();
   const { isSubapp, theme, setTheme } = useAppSettings();
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <Flex
       flexDir="column"
       w={isSubapp ? "100%" : "100dvw"}
       h={isSubapp ? "100%" : "100dvh"}
     >
-      <Flex justifyContent="end" alignItems="center" p={2}>
+      <Flex alignItems="center" p={2}>
+        {location.pathname !== "/" && (
+          <Button
+            variant="ghost"
+            leftIcon={<ArrowLeft size={32} />}
+            onClick={() => navigate(-1)}
+          >
+            Go back
+          </Button>
+        )}
         {!isSubapp && (
           <Box
+            ml="auto"
             bg="linear-gradient(90deg, hsla(139, 72%, 83%, 1) 0%, hsla(229, 89%, 62%, 1) 100%)"
             p={1}
             borderRadius="8px"
           >
             <Flex gap={2} borderRadius="8px">
               <VisuallyHidden>{t("appSettings")}</VisuallyHidden>
-              <>
-                <Select
-                  w="auto"
-                  bg="gray.800"
-                  color="whiteAlpha.800"
-                  aria-label={t("selectLanguage")}
-                  value={i18n.language}
-                  onChange={(e) => i18n.changeLanguage(e.target.value)}
-                >
-                  <option value="en-US">en-US</option>
-                  <option value="pt-BR">pt-BR</option>
-                  <option value="es-ES">es-ES</option>
-                </Select>
-                <Tooltip placement="left" label={t("changeTheme")}>
-                  <IconButton
-                    aria-label={t("changeTheme")}
-                    color="gray.800"
-                    bg="transparent"
-                    _hover={{ bg: "whiteAlpha.400" }}
-                    onClick={() =>
-                      setTheme(theme === "dark" ? "light" : "dark")
-                    }
-                    icon={
-                      theme === "dark" ? (
-                        <SunHorizon size={36} weight="fill" />
-                      ) : (
-                        <MoonStars size={36} weight="fill" />
-                      )
-                    }
-                  />
-                </Tooltip>
-              </>
+              <Select
+                w="auto"
+                bg="gray.800"
+                color="whiteAlpha.800"
+                aria-label={t("selectLanguage")}
+                value={i18n.language}
+                onChange={(e) => i18n.changeLanguage(e.target.value)}
+              >
+                <option value="en-US">en-US</option>
+                <option value="pt-BR">pt-BR</option>
+                <option value="es-ES">es-ES</option>
+              </Select>
+              <Tooltip placement="left" label={t("changeTheme")}>
+                <IconButton
+                  aria-label={t("changeTheme")}
+                  color="gray.800"
+                  bg="transparent"
+                  _hover={{ bg: "whiteAlpha.400" }}
+                  onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                  icon={
+                    theme === "dark" ? (
+                      <SunHorizon size={36} weight="fill" />
+                    ) : (
+                      <MoonStars size={36} weight="fill" />
+                    )
+                  }
+                />
+              </Tooltip>
             </Flex>
           </Box>
         )}
